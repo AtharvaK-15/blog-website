426bdab9907383089a77802a081887604cd9b183
atharva <atharvak.8023@gmail.com>
2023-10-05 17:47:42 +0530

    fixed bug

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.get("/posts/:AnyName",function(req,res){
 //   ContentDay: content});
 // })
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3004, function() {
+  console.log("Server started on port 3004");
 });
